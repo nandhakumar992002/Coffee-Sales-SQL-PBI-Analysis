a8437014e6de7202789519617f29e965b6f293e8
NandhaKumar <nanda9092400667@gmail.com>
2025-12-11 18:52:32 +0530

    Customer Insight.sql

--- sql-scripts/Customer Insight.sql
+++ sql-scripts/Customer Insight.sql
@@ -4,3 +4,12 @@ FROM coffeesales
 GROUP BY card
 HAVING card <> '' 
 ORDER BY COUNT(card) DESC;
+
+-- Top 10 Customers (Card Users)
+
+SELECT card, ROUND(SUM(money), 1) AS RevFrmCus,CONCAT(ROUND((SUM(money)/ (Select Sum(money) From coffeesales))*100, 1), "%") AS contribution_to_totalrev, COUNT(card) AS TimesBought
+FROM coffeesales
+WHERE card 	IS NOT NULL AND card <> ''
+GROUP BY card
+ORDER BY RevFrmCus DESC 
+LIMIT 10 ;
